449891b74dfbf12130e82788b72cfc755eb7381a
yazanAz20-meet <yazan7azaizah@gmail.com>
2024-03-05 20:37:33 +0200

    order

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ config = {
   "storageBucket": "hestiapoc.appspot.com",
   "messagingSenderId": "874274274779",
   "appId": "1:874274274779:web:254544ff8b5c83e37f3bfe",
-  "measurementId": "G-QSENWPXDP6"}
+  "measurementId": "G-QSENWPXDP6",
+  "databaseURL": "https://hestiapoc-default-rtdb.europe-west1.firebasedatabase.app/"}
+
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
@@ -16,3 +18,8 @@ db = firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
+
+#all the code down
+@app.route("/",methods=['Get','Post'])
+def homepage():
+    return render_template("index.html")
